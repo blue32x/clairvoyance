ea169395b0f6407a5f2e8a61999139a856e2c08c
Choi sung hyeun <caramalnim@naver.com>
2015-05-04 21:11:56 +0900

    fire punch

--- src/window/kinect_v2_test/kinect_v2_test/kinect_v2_test_main.cpp
+++ src/window/kinect_v2_test/kinect_v2_test/kinect_v2_test_main.cpp
@@ -24,249 +24,22 @@ inline void SafeRelease( Interface *& pInterfaceToRelease )
 
 #define depthWidth 512
 #define depthHeight 424
-/*
-//http://www.buildinsider.net/small/kinectv2cpp/02
-int main()
-{
-	//Sensor를 얻을 수 있다.
-	///////////////////////////////////////////////////////////////////////////////////
-	IKinectSensor * pSensor;  //Kinect v2 대우를 위한 Sensor인터페이스.
-	HRESULT hResult = S_OK;
-	hResult = GetDefaultKinectSensor(&pSensor);  //기본 Sensor를 얻을 수 있다.
-	if(FAILED(hResult))
-	{
-		std :: cerr << "Error : GetDEfaultKinectSensor" << std ::endl;
-		return -1;
-	}
-	hResult = pSensor ->Open(); //Sensor를 연다.
-	if(FAILED(hResult))
-	{
-		std :: cerr << "Error : IKinectSensor :: opne()" << std :: endl;
-	}
-	////////////////////////////////////////////////////////////////////////////////////
+#define colorWidth 1920
+#define colorHeight 1080
 
 
-	//Sensor에서 Source를 가져온다.
-	////////////////////////////////////////////////////////////////////////////////////
-	IColorFrameSource * pColorSource; //color 구조를 위한 Source 인터페이스
-	hResult = pSensor -> get_ColorFrameSource(&pColorSource); //Sensor에서 Source를 가져온다.
-	if(FAILED(hResult)) //pColorSource에 kinect v2의 color 정보가 저장된 상태이다.
-	{
-		std::cerr << "Error : IKinectSensor :: get_ColorFrameSource()" << std:: endl;
-		return -1;
-	}
-
-	IDepthFrameSource * pDepthSource; //depth 구조를 위한 Source 인터페이스
-	hResult = pSensor -> get_DepthFrameSource(&pDepthSource);
-	if(FAILED(hResult))
-	{
-		std::cerr<<"Error :IKinectSensor::get_DepthFrameSource()"<<std::endl;
-		return -1;
-	}
-	////////////////////////////////////////////////////////////////////////////////////
-
-
-	//Source에서 Reader를 연다.
-	////////////////////////////////////////////////////////////////////////////////////
-	IColorFrameReader *pColorReader; //Color 구조를 위한 Rader 인터페이스.
-	hResult = pColorSource -> OpenReader( & pColorReader); //Source에서 Reader를 연다.
-	if(FAILED(hResult))
-	{
-		std :: cerr << "Error : IColorFrameSource :: OpenReader()" << std :: endl;
-		return -1;
-	}
-	IDepthFrameReader * pDepthReader; //depth 구조를 위한 Reader 인터페이스
-	hResult = pDepthSource -> OpenReader( & pDepthReader); //Source에서 Reader를 연다.
-	if(FAILED(hResult))
-	{
-		std::cerr << "Error : IDepthFrameSource :: OpenReader()"<<std::endl;
-		return -1;
-	}
-	////////////////////////////////////////////////////////////////////////////////////
-
-
-	//Frame ~ Data
-	//color
-	////////////////////////////////////////////////////////////////////////////////////
-	int colorWidth = 1920; //image의 크기
-	int colorHeight = 1080;
-	unsigned int colorBufferSize = colorWidth * colorHeight * 4 * sizeof(unsigned char); //color 이미지의 테이터 크기.
-
-	//Color 이미지를 처리하기 위해 OpenCV의 cv::Mat형을 준비한다.
-	//bufferMat 원시 이미지 테이터 colorMat는 리사이즈 한 화상 데이터를 취급힌다.
-	//CV_8UC4는 부호없는 8bit정수가 4channel 나란히 1화소를 표현하는 데이터 형식이다.
-	cv::Mat colorBufferMat(colorHeight, colorWidth, CV_8UC4); 
-	
-	/////////////////////////////////////////////////////////////////////////////////////
-
-
-
-	//Frame ~ Data
-	//depth
-	//////////////////////////////////////////////////////////////////////////////////////
-	
-	int depthWidth = 512;
-	int depthHeight= 424;
-	unsigned int depthBufferSize = depthWidth * depthHeight * sizeof(unsigned short);
-
-	//Depth 데이터를 처리하기 위해 OpendCV의 cv::Mat 형을 준비한다.
-	//bufferMat는 16bit 원시 Depth 데이터 depthMat는 이미지로 표기하기 위해 8bit의 범위에 담는 Depth데이터를 취급한다.
-	//"CV_16UC1"은 부호없는 16bit 정수 (16U)이 1channel (C1) 나란히 1 화소를 표현하는 데이터 형식이다. 
-	//"CV_8UC1"은 부호없는 8bit 정수 (8U)를 표현하는 데이터 형식이다.
-	cv :: Mat depthBufferMat(depthHeight, depthWidth, CV_16UC1);
-	
-	//////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-	//get Coordinate Mapper
-	///////////////////////////////////////////////////////////////////////////////////////
-	ICoordinateMapper * pCoordinateMapper;
-	hResult = pSensor-> get_CoordinateMapper(&pCoordinateMapper);
-	if(FAILED(hResult))
-	{
-		std::cerr << "Error : IKinectSensor::get_CoordinateMapper()" << std::endl;
-		return -1;
-	}
-
-	cv::Mat coordinateMapperMat(depthHeight,depthWidth,CV_8UC4);
-	cv::namedWindow("CoordinateMapper");
-	///////////////////////////////////////////////////////////////////////////////////////
-
-
-	unsigned short minDepth, maxDepth;
-	pDepthSource->get_DepthMinReliableDistance(&minDepth);
-	pDepthSource->get_DepthMaxReliableDistance(&maxDepth);
-
-
-
-	//Frame을 생성하고 color와 depth image를 출력한다.
-	///////////////////////////////////////////////////////////////////////////////////////
-	while(1)
-	{
-		//colorFrame
-		IColorFrame * pColorFrame = nullptr; //color 이미지를 얻기 위한 Frame 인터페이스
-		hResult = pColorReader -> AcquireLatestFrame(&pColorFrame); //Reader에서 최신 Frame을 얻을 수 있다.
-		if(SUCCEEDED(hResult))
-		{
-			//Frame에서 Color이미지를 얻을 수 있다.
-			//기본 형식은 YUY2(=밝기와 색상 차이로 표현하는 형식)이지만, 다루기 쉬운 BGRA로 변환하여 얻을 수 있다.
-			pColorFrame -> CopyConvertedFrameDataToArray(colorBufferSize, reinterpret_cast<BYTE*> (colorBufferMat.data),ColorImageFormat_Bgra);
-		}
-
-
-		//depthFrame
-		IDepthFrame * pDepthFrame = nullptr;
-		hResult = pDepthReader -> AcquireLatestFrame(&pDepthFrame);
-		if(SUCCEEDED(hResult))
-		{
-			//Frame에서 Depth데이터를 검색한다.
-			//Depth 데이터가 저장된 배열의 포인터를 얻을 수 있다. 여기서 Depth 데이터를 시각화 하기 위한 변환처리에 편리한 cv::Mat 형으로 받고있다.
-			pDepthFrame -> AccessUnderlyingBuffer(&depthBufferSize, reinterpret_cast<UINT16**> (&depthBufferMat.data));
-			
-		}
-
-
-
-		//Mapping (Depth to Color)
-		std::vector<ColorSpacePoint> colorSpacePoints(depthWidth * depthHeight);
-		//vectro는 배열을 대신하여 사용 할 수 있는 자료구조
-		//배열의 크기는 정적이지만 vector는 동적으로 크기가 변한다.
-		//vector는 중간 자료의 삭제나 삽입이 없다.
-		hResult = pCoordinateMapper->MapDepthFrameToColorSpace(depthWidth * depthHeight, reinterpret_cast<UINT16 *>( depthBufferMat.data ), depthWidth * depthHeight, &colorSpacePoints[0] );
-		//depthWidth * depthHight 크기의 자료인 depthBufferMat.data를 
-		//depthWidth * depthHight 크기의 자료인 colorSpacePoints의 (ColorSpacePoint 자료형 벡터) 첫번째 주소부터 넣는다.
-		//결과는 colorSpacePoints 벡터에 depth 자료가 들어가게 된다.
-		if(SUCCEEDED(hResult))
-		{
-			coordinateMapperMat = cv::Scalar(0,0,0,0);
-			//Mat를 상속받는??? Scalar는 4개의 값만 사용하는 data type
-			//각각의 원소는 double형인듯?? 한데 더 찾아봐야할듯
-			//rgb 나타내려는듯!?
-
-			//
-			//x y 좌표계의 기준을 더 찾아볼것
-			for(int y = 0; y < depthHeight; y++)
-			{
-				for(int x = 0; x < depthWidth; x++)
-				{
-<<<<<<< HEAD
-					unsigned int index = y * depthWidth + x;
-					ColorSpacePoint point = colorSpacePoints[index];
-
-					// round down to the nearest pixel
-					int colorX = static_cast<int>(std::floor(point.X + 0.5));
-					int colorY = static_cast<int>(std::floor(point.Y + 0.5));
-=======
-					unsigned int index = y * depthWidth + x; //vector가 1차원 배열이기 때문에 index값을 지금과 같이 설정해 준것임
-					ColorSpacePoint point = colorSpacePoints[index]; //point 값에 index에 해당하는 값을 대입한다.
-
-					//int로의 자료 변환
-					int colorX = static_cast<int>(std::floor(point.X));
-					int colorY = static_cast<int>(std::floor(point.Y));
-
-					//depth 정보를 unsigned short 자료형으로 받아 온다.
->>>>>>> 4d32d69fca6290a0812c9031b7584423d33c33f4
-					unsigned short depth = depthBufferMat.at<unsigned short>(y,x);
-					
-
-					// make sure the pixel is part of the image
-					if( ( colorX >= 0 ) && ( colorX < colorWidth ) && ( colorY >= 0 ) && ( colorY < colorHeight ))// && ( depth >= minDepth ) && ( depth <= maxDepth ) )
-					{
-						coordinateMapperMat.at<cv::Vec4b>( y, x ) = colorBufferMat.at<cv::Vec4b>( colorY, colorX );
-					}
-					
-
-				}
-			}
-		}
-
-
-
-
-
-		SafeRelease(pDepthFrame); 
-		SafeRelease(pColorFrame); //Frame을 풀어놓는다.
-		//내부 버퍼가 해제되어 다음 데이터를 검색 할 수있는 산태가 된다.
-
-		//Show window
-		
-		cv::imshow("CoordinateMapper",coordinateMapperMat);
-		if( cv::waitKey( 30 ) == VK_ESCAPE )
-		{
-			break;
-		}
-	}
-	////////////////////////////////////////////////////////////////////////////////////
-
-	SafeRelease( pColorSource );
-	SafeRelease( pColorReader );
-	SafeRelease( pDepthSource );
-	SafeRelease( pDepthReader );
-	SafeRelease( pCoordinateMapper);
-	//kinect sensor close
-	if( pSensor )
-	{
-		pSensor->Close();
-	}
-
-	//kinect sensor pointer release
-	SafeRelease( pSensor );
-
-	cv::destroyAllWindows(); //생성한 3개의 윈도우를 닫아준다.
-
-
-	return 0;
-}
-
-*/
+//coordinate 이미지 영상을 줄여주는 변수
+//1로 바꿔주면 원본, 1이상이면 영상의 크기를 줄여주어 
+//속도를 빠르게 해준다.
+//추후에 멀티쓰레드 혹은 분산처리로 해결 가능성을 보인다.
+#define SPEEDBOOST 4
 
 
 //color + depth 영상 출력 
 //http://www.buildinsider.net/small/kinectv2cpp/02
 int main()
 {
+
 	//Sensor를 얻을 수 있다.
 	///////////////////////////////////////////////////////////////////////////////////
 	IKinectSensor * pSensor;  //Kinect v2 대우를 위한 Sensor인터페이스.
@@ -327,8 +100,7 @@ int main()
 	//Frame ~ Data
 	//color
 	////////////////////////////////////////////////////////////////////////////////////
-	int colorWidth =1920; //image의 크기
-	int colorHeight = 1080;
+	
 	unsigned int colorBufferSize = colorWidth * colorHeight * 4 * sizeof(unsigned char); //color 이미지의 테이터 크기.
 
 	//Color 이미지를 처리하기 위해 OpenCV의 cv::Mat형을 준비한다.
@@ -337,7 +109,7 @@ int main()
 	cv::Mat colorBufferMat(colorHeight, colorWidth, CV_8UC4); 
 	cv::Mat colorMat(colorHeight/2, colorWidth/2, CV_8UC4);
 	
-	cv::namedWindow("Color");
+	cv::namedWindow("Output");
 	/////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -356,7 +128,7 @@ int main()
 	//"CV_8UC1"은 부호없는 8bit 정수 (8U)를 표현하는 데이터 형식이다.
 	cv :: Mat depthBufferMat(depthHeight, depthWidth, CV_16UC1);
 	cv :: Mat depthMat(depthHeight, depthWidth, CV_8UC1);
-	cv :: namedWindow("Depth");
+	
 	
 	//////////////////////////////////////////////////////////////////////////////////////
 
@@ -373,8 +145,12 @@ int main()
 		return -1;
 	}
 
-	cv::Mat coordinateMapperMat(depthHeight,depthWidth,CV_8UC4);
-	cv::namedWindow("CoordinateMapper");
+
+	//color에 depth를 표현하기 위한 메트릭스
+	cv::Mat colorCoordinateMapperMat(colorHeight/SPEEDBOOST,colorWidth/SPEEDBOOST,CV_8UC4);
+
+
+	
 	///////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -420,45 +196,89 @@ int main()
 		}
 
 
-		//Mapping Frame
-		//  using arry
-		ColorSpacePoint colorSpacePoints[depthHeight][depthWidth]; //coordinate 정보를 저장할 2차원 배열을 만든다.
-		hResult = pCoordinateMapper->MapDepthFrameToColorSpace(depthWidth * depthHeight, reinterpret_cast<UINT16 *>(depthBufferMat.data), depthWidth * depthHeight, &colorSpacePoints[0][0]);
-		//colorSpacePoints 정보에 color 정보는 넣는다.
+
+		//Mapping Frame	
+		//color에 depth를 표현
+		DepthSpacePoint depthSpacePoints[colorHeight][colorWidth];
+		hResult = pCoordinateMapper->MapColorFrameToDepthSpace(depthWidth * depthHeight, reinterpret_cast<UINT16 *>(depthBufferMat.data), colorWidth * colorHeight, &depthSpacePoints[0][0]);
 		if(SUCCEEDED(hResult))
 		{
-			//sclar (4_)
-			coordinateMapperMat = cv::Scalar(0,0,0,0);
-			for(int y = 0; y < depthHeight; y++)
+			colorCoordinateMapperMat = cv::Scalar(0,0,0,0);
+		
+			
+			for(int y = 0; y < colorHeight; y+=SPEEDBOOST)
 			{
-				for(int x = 0; x < depthWidth; x++)
+				for(int x = 0; x < colorWidth; x+=SPEEDBOOST)
 				{
-					ColorSpacePoint point = colorSpacePoints[y][x];
+					DepthSpacePoint dPoint = depthSpacePoints[y][x];
 
-					int colorX = static_cast<int>(std::floor(point.X+0.5));
-					int colorY = static_cast<int>(std::floor(point.Y+0.5));
-					unsigned short depth = depthBufferMat.at<unsigned short>(y,x);
+					int depthX = static_cast<int>(dPoint.X);
+					int depthY = static_cast<int>(dPoint.Y);
+					colorCoordinateMapperMat.at<cv::Vec4b>(y/SPEEDBOOST,x/SPEEDBOOST) = colorBufferMat.at<cv::Vec4b>(y,x);
+					
+					
+				}
+			}
+			
+		}
 
-					if( (colorX >= 0) && ( colorX < colorWidth) && ( colorY >= 0) && (colorY < colorHeight) && (depth >= minDepth) && ( depth <= maxDepth))
+
+
+		//hiv이미지로 변환
+		
+		cv::Mat hsvimage(colorHeight/SPEEDBOOST,colorWidth/SPEEDBOOST,CV_8UC4);
+		cv::cvtColor(colorCoordinateMapperMat, hsvimage, CV_BGR2HSV,4);
+
+
+		///his이미지 변환을 통한 color 이미지 처리
+		
+		for(int y = 0; y < colorHeight; y+=SPEEDBOOST)
+		{
+			for(int x = 0; x < colorWidth; x+=SPEEDBOOST)
+			{
+				DepthSpacePoint dPoint = depthSpacePoints[y][x];
+				int depthX = static_cast<int>(dPoint.X);
+				int depthY = static_cast<int>(dPoint.Y);
+				if(depthX >=0 && depthX < depthWidth && depthY >= 0 && depthY < depthHeight)
+				{
+					
+					if(depthMat.at<uchar>(depthY,depthX)  >= 235)
 					{
-						coordinateMapperMat.at<cv::Vec4b>(y,x) = colorBufferMat.at<cv::Vec4b>(colorY,colorX);
+						
+						//hsvimage.at<cv::Vec4b>(y/SPEEDBOOST,x/SPEEDBOOST)[2] = 0.1;
+						colorCoordinateMapperMat.at<cv::Vec4b>(y/SPEEDBOOST,x/SPEEDBOOST)[2] = 250;
+						//colorCoordinateMapperMat.at<cv::Vec4b>(y/SPEEDBOOST,x/SPEEDBOOST)[0] =0;
+						//colorCoordinateMapperMat.at<cv::Vec4b>(y/SPEEDBOOST,x/SPEEDBOOST)[1] = 0;
 					}
+					
 				}
 			}
 		}
 		
+		
 
 
 
+
+		//hiv -> bgr 이미지로 변환
+		//cv::cvtColor(hsvimage, colorCoordinateMapperMat, CV_HSV2BGR,4);
+		
+		
+		cv::resize(colorCoordinateMapperMat,colorMat,cv::Size(),1,1);
+		
+	
 
 		SafeRelease(pDepthFrame); 
 		SafeRelease(pColorFrame); //Frame을 풀어놓는다.
 		//내부 버퍼가 해제되어 다음 데이터를 검색 할 수있는 산태가 된다.
 
 		//Show window
-		cv::imshow("Color", colorMat);
+		cv::imshow("Output", colorMat);
 		cv::imshow("Depth", depthMat);
-		cv::imshow("CoordinateMapper",coordinateMapperMat);
+		//cv::imshow("CoordinateMapper",colorCoordinateMapperMat);
+
+		//break;
+
 		if( cv::waitKey( 30 ) == VK_ESCAPE )
 		{
 			break;
